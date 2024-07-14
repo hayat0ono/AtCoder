6f1de136016a465e105ee41a5a6336cac99d8c83
hayat0ono <oppi_titan@icloud.com>
2024-07-15 02:44:54 +0900

    make algo templates

--- algorithm/templates.py
+++ algorithm/templates.py
@@ -1,27 +1,3 @@
-# inputs
-'''
-# abcde => 'abcde'
-s = input()
-
-# abcde = ['a','b','c','d','e']
-s = list(input())
-
-# 5 => 5
-a = int(input())
-
-# 1 2 => 1, 2
-x,y = map(int,input().split())
-
-# 1 2 3 4 5 ... n => ['1','2','3','4','5',...,'n']
-li = input().split()
-
-# 1 2 3 4 5 ... n => [1,2,3,4,5,...,n]
-li = list(map(int,input().split()))
-
-# FFFTFTTFF => ['FFF', 'F', '', 'FF']
-li = input().split('T')
-'''
-
 # binary search 1
 '''
 import bisect
